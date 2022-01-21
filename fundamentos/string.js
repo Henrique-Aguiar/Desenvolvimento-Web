7a46ee435cc85cfcdc1c69442a290e626401affe
Henrique Aguiar <hrikelr@hotmail.com>
2022-01-21 19:30:24 -0300

    conceitos basicos

---
+++ fundamentos/string.js
@@ -0,0 +1,8 @@
+const escola = 'Cod3r'
+
+console.log(escola.charAt(4)) // Letra que esta no índice 4
+console.log(escola.indexOf('d')) // Índice que esta o 'd'
+
+console.log(escola.substring(0, 3))
+console.log('Escola '.concat(escola) + ('!'))
+console.log(escola.replace('3', 'e')) // subistitui o 3 pelo e
