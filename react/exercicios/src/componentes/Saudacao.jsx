b804b1ef9c36e40058b42010fb8efb32cd59263f
Henrique-Aguiar <hrikelr@hotmail.com>
2022-05-19 21:10:48 -0300

    introdução a react

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-export default class Saldacao extends Component {
+export default class Saudacao extends Component {
 
   state = { 
     tipo: this.props.tipo,
@@ -28,7 +28,7 @@ export default class Saldacao extends Component {
         <h1>{tipo} {nome}!</h1>
         <hr />
         <input type="text" placeholder="Tipo..." value={tipo}
-          onChange={e => this.setTipo(e)} />
+          onChange={this.setTipo} />
         <input type="text" placeholder="Nome..." value={nome}
           onChange={e => this.setNome(e)}/>
       </div>
