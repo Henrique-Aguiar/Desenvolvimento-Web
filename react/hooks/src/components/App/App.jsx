df2ac815d899d57fb8de6b5124252386ebd44f04
Henrique-Aguiar <hrikelr@hotmail.com>
2022-09-12 19:58:28 -0300

    componente sem estado

---
+++ react/hooks/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from '../Counter/Counter';
+
+function App() {
+  return (
+    <>
+      <h1>Hello word!</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
