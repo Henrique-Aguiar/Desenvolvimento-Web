3d2125a33873ade4045bb66fc530d161f175c18f
Henrique-Aguiar <hrikelr@hotmail.com>
2022-10-06 17:20:43 -0300

    useReducer

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,12 +1,37 @@
+import { useReducer } from 'react';
 import './App.css';
-import { Div } from './components/Div';
-import { AppContext } from './contexts/AppContext';
+
+const globalState = {
+  title: 'O titulo que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      return { ...state, title: 'mudou' };
+    }
+    case 'inverter': {
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { title, body, counter } = state;
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <p>{body}</p>
+      <button onClick={() => dispatch({ type: 'muda' })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+    </div>
   );
 }
 
