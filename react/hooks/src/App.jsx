908cd0bc841283ad69e8b258c074c5167e976a1b
Henrique-Aguiar <hrikelr@hotmail.com>
2022-10-05 21:48:21 -0300

    hook useContext

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -1,51 +1,12 @@
-import React, { useContext, useState } from 'react';
 import './App.css';
-
-const globalState = {
-  title: 'O titulo que contexto',
-  counter: 0,
-  body: 'O body do contexto',
-};
-
-const GlobalContext = React.createContext();
-
-function Div() {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-}
-
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title, counter },
-  } = theContext;
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  );
-};
-
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { body },
-    setContextState,
-  } = theContext;
-  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
-};
+import { Div } from './components/Div';
+import { AppContext } from './contexts/AppContext';
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
-
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
---
+++ react/hooks/src/contexts/AppContext/index.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from 'react';
+import { globalState } from './data';
+
+export const GlobalContext = createContext();
+
+export const AppContext = ({ children }) => {
+  const [contextState, setState] = useState(globalState);
+  return <GlobalContext.Provider value={{ contextState, setState }}>{children}</GlobalContext.Provider>;
+};
