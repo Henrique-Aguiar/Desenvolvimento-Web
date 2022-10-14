bd3124fb44d575ca7d776655a8b4683e7e302326
Henrique-Aguiar <hrikelr@hotmail.com>
2022-10-13 22:25:28 -0300

    custom hook

--- react/exercicios/src/App.jsx
+++ react/exercicios/src/App.jsx
@@ -1,98 +1,43 @@
-import { useEffect, useRef, useState } from 'react';
-
-const isObjectEqual = (obj1, obj2) => {
-    return JSON.stringify(obj1) === JSON.stringify(obj2);
-}
-
-const useFetch = (url, options) => {
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    let changed = false;
-
-    if (!isObjectEqual(url, urlRef.current)) {
-        urlRef.current = url
-        changed = true;
-    }
-
-    if (!isObjectEqual(options, optionsRef.current)) {
-        optionsRef.current = options
-        changed = true;
-    }
-
-    if (changed) {
-        setShouldLoad((s) => !s);
-    }
-  }, [url, options]);
-
-  useEffect(() => {
-    console.log('EFFECT', new Date().toLocaleString());
-    console.log(optionsRef.current.headers);
-
-    setLoading(true);
-
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000));
-
-      try {
-        const response = await fetch(urlRef.current, optionsRef.current);
-        const jsonResult = await response.json();
-        setResult(jsonResult);
-        setLoading(false);
-      } catch (e) {
-        setLoading(false);
-        throw e;
-      }
-    };
-
-    fetchData();
-  }, [shouldLoad]);
-
-  return [result, loading];
-};
+import { useEffect, useState } from 'react';
+import { useFetch } from './use-fetch';
 
 export const App = () => {
-  const [PostId, setPostId] = useState('')   
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + PostId,
-  {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
     headers: {
-        abc: '6',
-    }
-  }
-);
+      abc: 7 + postId,
+    },
+  });
 
   useEffect(() => {
-    console.log('Id do post', PostId)
-  }, [PostId])
+    console.log('ID do post', postId);
+  }, [postId]);
 
   if (loading) {
     return <p>Loading...</p>;
   }
 
   const handleClick = (id) => {
-    setPostId(id)
-  } 
+    setPostId(id);
+  };
 
   if (!loading && result) {
+    // 1
     return (
-        <div>
-            {result?.length > 0 ? (
-                result.map((p) => (
-                    <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
-                        <p>{p.title}</p>
-                    </div>
-                ))
-            ) : (
-                <div onClick={() => handleClick('')}>
-                    <p>{result.title}</p>
-                </div>
-            )}
-        </div>
-    )
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
   }
 
   return <h1>Oi</h1>;
