3df04af3a1d00b7bc2628b7fa2723f91eb62803e
Henrique-Aguiar <hrikelr@hotmail.com>
2022-10-24 20:16:58 -0300

    React.lazy Code-Splitting

--- react/exercicios/src/App.jsx
+++ react/exercicios/src/App.jsx
@@ -1,44 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useFetch } from './use-fetch';
+import React, { Suspense, useState } from 'react';
+// import LazyComponent from './lazy-component';
+
+const loadComponent = () => {
+  console.log('Componente carregando...');
+  return import('./lazy-component');
+};
+const LazyComponent = React.lazy(loadComponent);
 
 export const App = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
-    headers: {
-      abc: 7 + postId,
-    },
-  });
+  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    console.log('ID do post', postId);
-  }, [postId]);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  const handleClick = (id) => {
-    setPostId(id);
-  };
-
-  if (!loading && result) {
-    // 1
-    return (
-      <div>
-        {result?.length > 0 ? (
-          result.map((p) => (
-            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
-              <p>{p.title}</p>
-            </div>
-          ))
-        ) : (
-          <div onClick={() => handleClick('')}>
-            <p>{result.title}</p>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  return <h1>Oi</h1>;
+  return (
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'LC on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando...</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
+  );
 };
