59ad76ff78b6dfcb03e795dbc2bf01779ead76a3
Henrique-Aguiar <hrikelr@hotmail.com>
2022-11-14 19:42:35 -0300

    test in button

--- react/context-api-and-hooks/src/templates/Home/index.jsx
+++ react/context-api-and-hooks/src/templates/Home/index.jsx
@@ -13,6 +13,16 @@ export const Home = () => {
       <h1 onClick={() => actions.increase()}>{state.counter}</h1>
       <div>
         <Button onButtonClick={() => actions.increase()}>Increase</Button>
+        <Button onButtonClick={() => actions.decrease()}>Decrease</Button>
+        <Button onButtonClick={() => actions.reset()}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>set10</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>set100</Button>
+        <Button disabled={state.loading} onButtonClick={() => actions.asyncIncrease()}>
+          async increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={() => actions.asyncError()}>
+          async error
+        </Button>
       </div>
     </div>
   );
