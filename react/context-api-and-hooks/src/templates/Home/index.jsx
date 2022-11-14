1160972581a3e122453f0dd3120499438b1923a2
Henrique-Aguiar <hrikelr@hotmail.com>
2022-11-14 19:01:38 -0300

    context api

--- react/context-api-and-hooks/src/templates/Home/index.jsx
+++ react/context-api-and-hooks/src/templates/Home/index.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { Button } from '../../components/Button';
+import { useCounterContext } from '../../contexts/CounterContext';
+
 export const Home = () => {
+  const [state, actions] = useCounterContext();
+
+  useEffect(() => {
+    actions.increase();
+  }, [actions]);
   return (
     <div>
-      <h1>oi</h1>
+      <h1 onClick={() => actions.increase()}>{state.counter}</h1>
+      <div>
+        <Button onButtonClick={() => actions.increase()}>Increase</Button>
+      </div>
     </div>
   );
 };
