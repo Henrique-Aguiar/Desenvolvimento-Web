5e80315ed64817855a1333ccf4afb90cfc2b2c74
Henrique-Aguiar <hrikelr@hotmail.com>
2022-11-16 18:44:26 -0300

    useHistory useLocation

--- react/router/src/template/Abc/index.jsx
+++ react/router/src/template/Abc/index.jsx
@@ -1,7 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 
 export const Abc = () => {
-    const params = useParams();
-    console.log(params);
-    return <h1>abc</h1>;
+    const { slug } = useParams();
+    const location = useLocation();
+    const history = useHistory();
+    useEffect(() => {
+        setTimeout(() => {
+            history.push('/');
+        }, 5000);
+    }, [history]);
+    console.log(location);
+    return <h1>abc {slug} </h1>;
 };
